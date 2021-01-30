674f465aae2df2fffa28fa2028b8214af7eaf16b
bimba9944 <64472768+bimba9944@users.noreply.github.com>
2021-01-30 19:59:13 +0100

    dodata tabela za filmove i ubacena dva filma kao proba

--- hw-4/baza.php
+++ hw-4/baza.php
@@ -47,6 +47,50 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         echo "Error creating table: " . $connect->error;
     }
 
+    $Tabela2 = "CREATE TABLE IF NOT EXISTS tabelaFilmova (
+        idF INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        naslovF VARCHAR(255) NOT NULL,
+        oFilmu VARCHAR(255) NOT NULL,
+        zanr VARCHAR(255) NOT NULL,
+        scenario VARCHAR(255) NOT NULL,
+        direktori VARCHAR(255) NOT NULL,
+        producenti VARCHAR(255) NOT NULL,
+        glumci VARCHAR(255) NOT NULL,
+        produkcija VARCHAR(255) NOT NULL,
+        poster VARCHAR(255) NOT NULL,
+        trajanje INT(255) NOT NULL,
+        ocena VARCHAR(255) NOT NULL,
+        brojOcena INT(255) NOT NULL
+    )";
+
+    if ($connect->query($Tabela2) === TRUE) {
+    // echo "Table Tabela2 created successfully";
+    } else {
+    echo "Error creating table: " . $connect->error;
+    }
+
+
+    $stmt1 = $connect->prepare("INSERT INTO tabelaFilmova (naslovF, oFilmu, zanr, scenario, direktori, producenti, glumci, produkcija, poster, trajanje, ocena, brojOcena) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt1->bind_param("ssssssssssss", $naslovF, $oFilmu, $zanr, $scenario, $direktori, $produkcija, $glumci, $godina, $poster, $trajanje, $ocena, $brojOcena);
+    $naslovF = "Star Wars: Episode IV - A New Hope";   //change with movie title
+    $oFilmu = "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire."; //change with movie description
+    $zanr = " Action, Adventure, Fantasy"; //change with movie genre
+    $scenario = "George Lucas"; //change with movie screenplay
+    $direktori = "George Lucas";  //change with movie director(s)
+    $produkcija = "Lucasfilm Ltd.";  //change with movie production
+    $glumci = " Mark Hamill, Harrison Ford, Carrie Fisher";  //change with movie starring
+    $godina = "1977";  //change with movie release year
+    $poster = "new_hope.png";  //change with movie picture name (picture is in ./images/)
+    $trajanje = 121;  //change with movie duration
+    $ocena = 0;  //change with movie grade (initialy 0)
+    $brojOcena = 0;     //change with movie numOfGrades (initialy 0)
+    $stmt1->execute(); 
+    $stmt1->close();
+
+
+
+
+
 
 
 
